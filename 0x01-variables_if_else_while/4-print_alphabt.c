5bf876a6b41b0bc03409e729173150fde305c01a
Obiukwu Uchechi Patence <obiukwupatience@gmail.com>
2022-10-14 19:59:18 -0700

    done

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * avoid:parameter
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	int;
+	for(i=97;i<123;i++)
+	{
+		if(i==101||1==113)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(i);
+		}
+	}
+	putchar(10);
+	return (0);
+}
