240fe16624e4a9a0364c6c032cdede7919f4c5df
Obiukwu Uchechi Patence <obiukwupatience@gmail.com>
2022-10-18 14:12:15 -0700

    high

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -5,7 +5,7 @@
  * @c: character to be checked
  * Return: 1 if character is a letter, 0 otherwise
  */
-int _is alpha(int c)
+int _isalpha(int c)
 {
 	if ((c >= 'a' && c <= 'z') || (c > 'A' && c < 'Z'))
 		return (1);
