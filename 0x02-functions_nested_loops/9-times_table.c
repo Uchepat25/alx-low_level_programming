0d4fd7f6c9078cc250d9b3c5ec5e1a2434691c78
Obiukwu Uchechi Patence <obiukwupatience@gmail.com>
2022-10-20 04:49:21 -0700

    air

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,6 +10,7 @@ void times_table(void)
 	for (num = 0; num <= 9; num++)
 	{
 		_putchar('0');
+
 		for (mult = 1; mult <= 9; mult++)
 		{
 			_putchar(',');
@@ -21,6 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + '0');
+
 			_putchar((prod % 10) + '0');
 		}
 			_putchar('\n');
